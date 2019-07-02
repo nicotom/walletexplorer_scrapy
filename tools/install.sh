dd0f479b5c61a55572828d82109fc85e964abc6b
Nicolas Tomatis <nicolas.tomatis.f@gmail.com>
2019-07-02 18:50:43 -0300

    Install conda

--- tools/install.sh
+++ tools/install.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+# Install conda if needed
+if ! [[ -x "$(command -v conda)" ]]; then
+    echo 'conda is not installed.' >&2
+
+    read -p "Proceed to download and install conda?" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        # Download Anaconda if not present
+        if [[ ! -f ~/miniconda.sh ]]; then
+            echo "Downloading conda"
+            wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+        fi
+
+        bash ~/miniconda.sh
+    else
+        echo 'Cannot continue without conda installed.'
+        exit 1
+    fi
+fi
+
 # Remove existing environment
 conda env remove --yes -n walletexplorer || echo 'Creating new environment'
 
