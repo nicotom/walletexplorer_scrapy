49924b1eb91cdfe2ac2cc679612acced11f31abf
Nicolas Tomatis <nicolas.tomatis.f@gmail.com>
2019-07-03 13:45:25 -0300

    Add various types of wallets

--- exchange_addresses/spiders/exchange_addresses.py
+++ exchange_addresses/spiders/exchange_addresses.py
@@ -2,47 +2,66 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "exchange_addresses"
+    name = "wallet_addresses"
     start_urls = [
         'https://www.walletexplorer.com/',
     ]
+    wallet_types = {
+        "exchange": 1,
+        "pool": 2,
+        "service": 3,
+        "gambling": 4,
+        "old": 5,
+    }
 
     def parse(self, response):
-        number_of_exchanges = len(response.xpath('//*[@id="main"]/table/tr/td[1]/ul/li').extract())
-        for exchange in range(1, number_of_exchanges + 1):
-            n_exchanges = getattr(self, 'n_exchanges', None)
-            if n_exchanges is not None:
-                if exchange > int(n_exchanges):
+        type_of_wallets = getattr(self, 'type', None)
+        if type_of_wallets in self.wallet_types:
+            wallet_type_name = type_of_wallets
+            wallet_type = self.wallet_types[type_of_wallets]
+        else:
+            wallet_type_name = 'default'
+            wallet_type = 1
+        self.logger.info('Query for Type of wallet "%s": %d' % (wallet_type_name, wallet_type))
+        number_of_wallets = len(response.xpath(f'//*[@id="main"]/table/tr/td[{wallet_type}]/ul/li').extract())
+
+        for wallet in range(1, number_of_wallets + 1):
+            n_wallets = getattr(self, 'n_wallets', None)
+            if n_wallets:
+                if wallet > int(n_wallets):
                     break
-            number_of_subexchanges = len(response.xpath(
-                f'//*[@id="main"]/table/tr/td[1]/ul/li[{exchange}]/a').extract())
-            for sub_exchange in range(1, number_of_subexchanges + 1):
+            number_of_subwallets = len(response.xpath(
+                f'//*[@id="main"]/table/tr/td[{wallet_type}]/ul/li[{wallet}]/a').extract())
+            for sub_wallet in range(1, number_of_subwallets + 1):
 
                 link = response.xpath(
-                        f'//*[@id="main"]/table/tr/td[1]/ul/li[{exchange}]/a[{sub_exchange}]/@href').extract()
+                        f'//*[@id="main"]/table/tr/td[{wallet_type}]/ul/li[{wallet}]/a[{sub_wallet}]/@href').extract()
                 absolute_url = QuotesSpider.start_urls[0] + link[0] + '/addresses'
-                exchange_name = response.xpath(
-                    f'//*[@id="main"]/table/tr/td[1]/ul/li[{exchange}]/a[{1}]/text()').extract()[0]
+                wallet_name = response.xpath(
+                    f'//*[@id="main"]/table/tr/td[{wallet_type}]/ul/li[{wallet}]/a[{1}]/text()').extract()[0]
                 yield response.follow(
                     absolute_url,
                     callback=self.parse_addresses,
                     meta={
                         'url': absolute_url,
-                        'exchange_name': exchange_name
+                        'wallet_name': wallet_name,
+                        'wallet_type': wallet_type_name
                     }
                 )
 
     def parse_addresses(self, response):
         addresses = response.xpath('//*[@id="main"]/table/tr[*]/td[1]/a/text()').getall()
         yield {
-            'exchange_name': response.meta.get('exchange_name'),
+            'wallet_type': response.meta.get('wallet_type'),
+            'wallet_name': response.meta.get('wallet_name'),
             'url': response.meta.get('url'),
             'addresses': addresses
         }
         next_page_url = response.xpath('//*[@id="main"]/div[2]/a[contains(text(),"Next")]/@href').get()
         n_pages = getattr(self, 'n_pages', None)
-        if int(next_page_url[-1]) > int(n_pages):
-            return
+        if n_pages:
+            if int(next_page_url[-1]) > int(n_pages):
+                return
         modified_meta = response.meta
         modified_meta['url'] = QuotesSpider.start_urls[0] + next_page_url
         yield response.follow(next_page_url, self.parse_addresses, meta=modified_meta)
--- tools/run.sh
+++ tools/run.sh
@@ -1,4 +1,78 @@
 #!/usr/bin/env bash
 
+# Getting options
+while getopts ":t:n:p:f:" opt;
+do
+    case $opt in
+        t) type="$OPTARG"
+        ;;
+        f) file="$OPTARG"
+        ;;
+        n) number="$OPTARG"
+        ;;
+        p) pages="$OPTARG"
+        ;;
+        \?) echo "https://www.walletexplorer.com Scraper"
+            echo ""
+            echo "Valid options:"
+            echo "-t : type of exchange. Options: exchange pool service gambling old"
+            echo "-f : path where to store the results. Default is tmp.json"
+            echo "-n : number of distinct wallets to scrape. By default scrapes all"
+            echo "-p : number of pages to scrape. By default scrapes all"
+            echo ""
+            echo "Example:"
+            echo "$0 -t exchange -f exchange_addresses.json -n 1 -p 2 "
+            exit 0
+        ;;
+    esac
+done
+
+function list_include_item {
+  list="$1"
+  item="$2"
+  if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
+    # yes, list include item
+    result=1
+  else
+    result=0
+  fi
+}
+
+if [[ -z  ${type} ]]
+then
+    type="exchange"
+fi
+list_include_item "exchange pool service gambling old" ${type}
+if [[ "$result" -eq "0" ]]
+then
+    echo "Unknown type of wallet type"
+    echo "Run $0 -h to see available parameters."
+    exit 1
+fi
+
+echo "Querying addresses of type \"$type\"."
+
+if [[ -z  ${file} ]]
+then
+    file='tmp.json'
+fi
+
+if [[ -z  ${number} ]]
+then
+    number=''
+else
+    echo "For a total of $number distinct."
+fi
+
+if [[ -z  ${pages} ]]
+then
+    pages=''
+else
+    echo "For $pages pages."
+fi
+echo "Saving results in \"$file\""
+
+echo "crawl wallet_addresses -t json -o - -a type=${type} -a n_wallets=${number} -a n_pages=${pages} > ${file}"
 source activate walletexplorer
-scrapy crawl exchange_addresses -t json -o - > addresses.json
+scrapy crawl wallet_addresses -t json -o - \
+    -a type=${type} -a n_wallets=${number} -a n_pages=${pages} > ${file}
